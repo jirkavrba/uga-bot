77dd2d6444091ac9359d2fc94a17ff8b63861975
Jiří Vrba <jiri@vrba.dev>
2020-08-06 07:38:52 +0200

    Added spring boot and Discord4J reactive Discord API

--- src/main/scala/dev/vrba/UgaBot/UgaBot.scala
+++ src/main/scala/dev/vrba/UgaBot/UgaBot.scala
@@ -1,8 +1,18 @@
 package dev.vrba.UgaBot
 
-object UgaBot {
-  def main(args: Array[String]): Unit = {
-    println("Hello there.")
+import org.springframework.boot.{CommandLineRunner, SpringApplication}
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class UgaBot extends CommandLineRunner {
+  // The application entry point
+  override def run(args: String*): Unit = {
+    println("Hello from Spring boot")
   }
 }
 
+object UgaBot {
+  def main(args: Array[String]): Unit = {
+    SpringApplication.run(classOf[UgaBot], args: _*)
+  }
+}
